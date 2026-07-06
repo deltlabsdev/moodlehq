baf3ea0512ced5e66696c8e942ec8d20f938bc5f
DeLT Labs <118813183+deltlabsdev@users.noreply.github.com>
2026-07-06 13:33:19 +0200

    Updates

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,20 @@
+
 #!/usr/bin/env bash
 set -e
 
 mkdir -p /var/www/moodledata
 chown -R www-data:www-data /var/www/moodledata
+
+CONFIG_FILE="/var/www/html/config.php"
+
+if [ -f "$CONFIG_FILE" ]; then
+    sed -i "s#\$CFG->wwwroot = .*#\$CFG->wwwroot = 'https://mdl.deltlabs.net';#" "$CONFIG_FILE"
+
+    if grep -q "sslproxy" "$CONFIG_FILE"; then
+        sed -i "s#\$CFG->sslproxy = .*#\$CFG->sslproxy = true;#" "$CONFIG_FILE"
+    else
+        sed -i "/\$CFG->wwwroot/a \$CFG->sslproxy = true;" "$CONFIG_FILE"
+    fi
+fi
 
 exec "$@"
